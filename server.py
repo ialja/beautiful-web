a34c1a0a69003334b60b3034ccc0dce59d94fa6a
Alja Isakovic <ialja@me.com>
2013-09-09 21:28:00 +0200

    added server side validation
    Added server side validation that works with JS disabled. Code still ugly, but it works

--- server.py
+++ server.py
@@ -33,19 +33,88 @@ def moar():
 @app.route("/complexicator", methods=['GET', 'POST'])
 def complexicator():
     if request.method == 'POST':
+        have_error = False
         age = request.form['age']
-        gender = request.form['gender']
-        status = request.form['status']
         ignorance = request.form['ignorance']
         money_have = request.form['money_have']
         money_wants = request.form['money_wants'] 
         money_spent = request.form['money_spent']
         popularity_online = request.form['popularity_online']
-        rl_friends = request.form['rl_friends']
 
-        money = (int(money_have) - int(money_spent)) - int(money_wants)
-        score = overall_score(int(age), int(gender), int(status), int(ignorance), money, int(popularity_online), int(rl_friends))
-        return render_template('complicator_result.html', complexity = score, level = check_level(score), interpretation = interpret_score(score))
+        error_msg = "Please fix the following errors: "
+        errors = dict(age = age,
+                        ignorance = ignorance,
+                        money_have = money_have,
+                        money_wants = money_wants,
+                        money_spent = money_spent,
+                        popularity_online = popularity_online,
+                        error_msg = error_msg)
+
+        # checking if checkboxes are filled in
+        if 'gender' in request.form:
+            gender = request.form['gender']
+            if gender.isdigit() and int(gender) in range (0, 11): 
+                errors['gender'] = gender
+            else:
+                have_error = True
+                errors['error_gender'] = "Trying to trick me?"
+        else:
+            have_error = True
+            errors['error_gender'] = "Pick your gender."
+
+        if 'status' in request.form:
+            status = request.form['status']
+            if status.isdigit() and int(status) in range (0, 11):
+                errors['status'] = status
+            else:
+                have_error = True
+                errors['error_status'] = "Trying to trick me?"
+        else:
+            have_error = True
+            errors['error_status'] = "Pick your status."
+
+        if 'rl_friends' in request.form:
+            rl_friends = request.form['rl_friends']
+            if rl_friends.isdigit() and int(rl_friends) in range (0, 11):                
+                errors['rl_friends'] = rl_friends
+            else:
+                have_error = True
+                errors['error_rl_friends'] = "Trying to trick me?"
+        else:
+            have_error = True
+            errors['error_rl_friends'] = "Pick the number of friends."
+
+
+        if not age.isdigit():
+            errors['error_age'] = "Age should be a whole number, written with digits."
+            have_error = True
+
+        if not ignorance.isdigit():
+            errors['error_ignorance'] = "Estimate your ignorance on a scale from 0 to 100."
+            have_error = True
+
+        if not money_have.isdigit():
+            errors['error_m_have'] = "Estimate the amount you currently have with numbers."
+            have_error = True
+
+        if not money_wants.isdigit():
+            errors['error_m_wants'] = "Estimate the amount you currently want with numbers."
+            have_error = True
+
+        if not money_spent.isdigit():
+            errors['error_m_spent'] = "Estimate the amount you spent today with numbers."
+            have_error = True
+
+        if not popularity_online.isdigit():
+            errors['error_klout'] = "Your Klout score is a number between 1 and 100. Write 0 if you don't know what this is."
+            have_error = True
+
+        if have_error:
+            return render_template('complicator_form.html', **errors)
+        else:
+            money = (int(money_have) - int(money_spent)) - int(money_wants)
+            score = overall_score(int(age), int(gender), int(status), int(ignorance), money, int(popularity_online), int(rl_friends))
+            return render_template('complicator_result.html', complexity = score, level = check_level(score), interpretation = interpret_score(score))
     else:
         return render_template('complicator_form.html')
 
